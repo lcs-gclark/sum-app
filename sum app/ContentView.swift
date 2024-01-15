164d369345ce609b3f055c12679719a280ab0cc6
grayden clark <gclark@lcs.on.ca>
2024-01-15 11:27:46 -0500

    Initial Commit

---
+++ sum app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sum app
+//
+//  Created by Gray Clark on 2024-01-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sum app/sum_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sum_appApp.swift
+//  sum app
+//
+//  Created by Gray Clark on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct sum_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
