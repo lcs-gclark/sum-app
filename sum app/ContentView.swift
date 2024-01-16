576c8491693da304295f53fcae606d737f73dc43
grayden clark <gclark@lcs.on.ca>
2024-01-16 14:10:27 -0500

    just need to find how to make buttons work

--- sum app/ContentView.swift
+++ sum app/ContentView.swift
@@ -1,19 +1,101 @@
 //
 //  ContentView.swift
-//  sum app
-//
-//  Created by Gray Clark on 2024-01-15.
+//  StepperExample
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var base1: Int = 1
+    
+    // MARK: Stored properties
+    @State var base2: Int = 1
+    
+    // MARK: Computed properties
+    var summed: Int {
+        return base1 + base2
+    }
+    var multiplyed: Int {
+        return base1 * base2
+    }
+    var subtracted: Int {
+        return base1 - base2
+    }
+    var divided: Int {
+        return base1 / base2
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+           
+            HStack {
+                Spacer()
+                Text("\(base1)")
+                    .font(.system(size: 90))
+            }
+            Stepper(value: $base1, label: {
+                Text("select first number")
+            })
+              
+Spacer()
+            HStack {
+                Text("+")
+                    .font(.system(size: 90))
+                Spacer()
+                
+                Text("\(base2)")
+                    .font(.system(size: 90))
+            }
+            Stepper(value: $base2, label: {
+                Text("select second number")
+            })
+            Spacer()
+
+            HStack {
+                Spacer()
+                Text("\(summed)")
+                    .font(.system(size: 90))
+            }
+         
+            HStack {
+                Button {} label: {
+                VStack {
+                Text("+")
+                Text("add").font(.body)
+                }
+                }
+                Spacer()
+                Button {} label: {
+                VStack {
+                Text("-")
+                Text("subtract").font(.body)
+                }
+                }
+                Spacer()
+                Button {} label: {
+                VStack {
+                Text("×")
+                Text("multiply").font(.body)
+                }
+                }
+                Spacer()
+                Button { var summed: Int {
+                    return base1 * base2
+                }} label: {
+                VStack {
+                Text("÷")
+                Text("divide").font(.body)
+                }
+                }
+            }
+            
+        
+            
         }
         .padding()
     }
@@ -22,3 +104,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
